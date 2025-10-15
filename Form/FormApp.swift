0f04962b52b06972baebb3991ba7a9d83f90aec1
therng <therngsupachai@icloud.com>
2025-10-15 20:29:55 +0700

    Initial Commit

---
+++ Form/FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormApp.swift
+//  Form
+//
+//  Created by Supachai Thawatchokthavee on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
